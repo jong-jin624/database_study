43529d2e383412ea228f1cefac2ba14b7c5b069e
jong-jin624 <jongjin1004baek@gmail.com>
2025-12-30 12:48:16 +0900

    퀴즈 7, 8

---
+++ quiz/quiz07.sql
@@ -0,0 +1,21 @@
+1.
+Student 테이블의 birthday 컬럼을 사용하여 생일이 1월인 학생의 이름과 birthday 를 출력하세요.
+select 
+    studno,
+    name,
+    --'19'|| replace(birthday, '/', '-') as birthday
+    TO_CHAR(birthday, 'YYYY-MM-DD') birthday
+from student
+where TO_CHAR(birthday, 'MM')='01';
+2.
+emp 테이블의 hiredate 컬럼을 사용하여 입사일이 1,2,3 월인 사람들의 사번과 이름, 입사일을 출력하세요.
+select
+    empno,
+    ename,
+    hiredate
+from emp
+where
+    TO_CHAR(hiredate, 'MM') IN ('01', '02', '03');
+--where TO_CHAR(hiredate, 'MM')='01' 
+--  or TO_CHAR(hiredate, 'MM')='02' 
+--  or TO_CHAR(hiredate, 'MM')='03';
